2d05e4b7da39e660b21c147a6d2c0c0b89b82856
Muhamad Hilmy Haidar <hilmy_21@apps.ipb.ac.id>
2021-12-24 00:19:23 +0700

    practice

---
+++ codeforces/1619A.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool DebugMode = false;
+
+typedef unsigned long long int ulli;
+typedef long long int lli;
+typedef unsigned long int uli;
+typedef long int li;
+typedef unsigned short int usi;
+typedef short int si;
+
+void printYesOrNo(bool v) {
+  if (v) printf("YES\n");
+  else printf("NO\n");
+}
+
+void printDebug(const char* cs, int x) {
+  if (DebugMode) printf("[%s]: %d\n", cs, x);
+} 
+
+void initMain(int argc, char** argv) {
+  if (argc > 1 && strcmp(argv[1], "debug") == 0) {
+    DebugMode = true;
+  }
+}
+
+void solve() {
+  string s;
+  cin >> s;
+
+  if (s.length() % 2) {
+    printYesOrNo(false);
+  } else {
+    bool valid = true;
+    size_t n = s.length()/2;
+    for (size_t i = 0; valid && i < n; i++) {
+      valid = s[i] == s[i+n];
+    }
+    printYesOrNo(valid);
+  }
+}
+
+int main(int argc, char** argv) {
+  initMain(argc, argv);
+
+  int t;
+  cin >> t;
+  while(t--) {
+    solve();
+  }
+  return 0;
+}
