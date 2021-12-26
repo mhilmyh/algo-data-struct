e7e870d1b921ac22d175b7307684ea18fbc2f14d
Muhamad Hilmy Haidar <hilmy_21@apps.ipb.ac.id>
2021-12-27 00:33:22 +0700

    practice

---
+++ codeforces/1421A.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool DebugMode = false;
+
+typedef unsigned long long int ulli;
+typedef long long int lli;
+typedef unsigned long int uli;
+typedef long int li;
+typedef unsigned short int usi;
+typedef short int si;
+
+void printYesOrNo(bool v) {
+  if (v) printf("YES\n");
+  else printf("NO\n");
+}
+
+void printDebug(const char* cs, int x) {
+  if (DebugMode) printf("[%s]: %d\n", cs, x);
+} 
+
+void initMain(int argc, char** argv) {
+  if (argc > 1 && strcmp(argv[1], "debug") == 0) {
+    DebugMode = true;
+  }
+}
+
+void solve() {
+  int a, b, c, d;
+  scanf("%d %d", &a, &b);
+
+  c = 0; d = 0;
+  while (a | b) {
+    c += ((a ^ b) & 1) << d;
+    a >>= 1;
+    b >>= 1;
+    d++;
+  }
+
+  printf("%d\n", c);
+}
+
+int main(int argc, char** argv) {
+  initMain(argc, argv);
+
+  int t;
+  cin >> t;
+  while(t--) {
+    solve();
+  }
+  return 0;
+}
